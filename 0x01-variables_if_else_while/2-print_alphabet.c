d46a42f7acc073aceee17a385858be4d50899415
oreoluwa27 <orework22@gmail.com>
2023-07-14 14:44:47 -0700

    3 task done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to print a to z in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
