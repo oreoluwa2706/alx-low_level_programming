fccb868386d1cde0126e082af18e6a47bcf27a94
oreoluwa27 <orework22@gmail.com>
2023-07-24 17:12:10 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
+/**
+ * main - This function generates a random valid password
+ * Return: void
+ */
+int main(void)
+{
+	int paswd[1000];
+	int a, sum, b;
+
+	sum = 0;
+	srand((unsigned int)time(NULL));
+
+	for (a = 0; a < 1000; a++)
+	{
+		paswd[a] = rand() % 78;
+		sum += (paswd[a] + '0');
+		putchar(paswd[a] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			b = 2772 - sum - '0';
+			sum += b;
+			putchar(b + '0');
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
 
 
