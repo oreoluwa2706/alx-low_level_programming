bddb70f7c9216ec5322b1e9bb83557c289ab7ae8
oreoluwa27 <orework22@gmail.com>
2023-08-04 15:36:44 -0700

    updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,41 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
-/**
- * coChanger - check for this function
- * @a: check for this parameter
- * Return: int
- */
-int coChanger(int a)
-{
-	int count = 0;
-
-	while (a != 0)
-	{
-		if (a % 10 == 9 || a % 10 == 7)
-			a -= 2;
-		else if (a % 25 == 0)
-			a -= 25;
-		else if (a % 10 == 0)
-			a -= 10;
-		else if (a % 5 == 0)
-			a -= 5;
-		else if (a % 2 == 0)
-		{
-			if (a % 10 == 6)
-				a -= 1;
-			else
-				a -= 2;
-		}
-		else
-			a -= 1;
-		count++;
-	}
-	return (count);
-}
 /**
  * main - check for this function
  * @argc: check for this parameter
@@ -44,26 +10,31 @@ int coChanger(int a)
  */
 int main(int argc, char *argv[])
 {
-	int a, co;
+	int i, j, ans;
 
-	co = 0;
+	int coin[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	i = atoi(argv[1]);
+	ans = 0;
 
-	a = atoi(argv[1]);
-
-	if (a < 0)
-		printf("0\n");
-	else
+	if (i < 0)
 	{
-		co = coChanger(a);
-
-		printf("%d\n", co);
+		printf("0\n");
+		return (0);
 	}
-
+	for (j = 0; j < 5 && i >= 0; j++)
+	{
+		while (i >= coin[j])
+		{
+			ans++;
+			i -= coin[j];
+		}
+	}
+	printf("%d\n", ans);
 	return (0);
 }
