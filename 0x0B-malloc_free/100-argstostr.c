eddc7fbb601dfa22e3384811fec8af267e12cfe2
oreoluwa27 <orework22@gmail.com>
2023-08-08 19:02:28 -0700

    updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -14,8 +13,6 @@ char *argstostr(int ac, char **av)
 	char *pt_str;
 
 	j = 0;
-	l = 0;
-
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
