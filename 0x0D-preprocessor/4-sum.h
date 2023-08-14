6ecfbc8a1afb6b0e5899a203fb3c51663cece199
oreoluwa27 <orework22@gmail.com>
2023-08-14 15:24:48 -0700

    updated

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,1 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * File: 4-sum.h
+ * Description: macro file
+ * @x: check for this parameter
+ * @y: check for this parameter
+ */
 #define SUM(x, y) (x + y)
+
+#endif
