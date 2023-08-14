6da47743699fc7ad303b45b9d7f33d583a245bd6
oreoluwa27 <orework22@gmail.com>
2023-08-14 16:06:52 -0700

    task 1 n 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - check for this function
+ * @d: check this parameter
+ * @name: check for this parameter
+ * @age: check for this parameter
+ * @owner: check for this parameter
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - check for this stuct type
+ * @name: checkk for this parameter
+ * @age: check for this parameter
+ * @owner: check for this parameter
+ * Decription: struct type
+ */
+typedef struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+
+#endif
